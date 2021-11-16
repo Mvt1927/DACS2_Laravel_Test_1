0c82d7ede8933ff6f70e15e8cfe54ef08b9b2e60
Mvt1927 <viettheog4@gmail.com>
2021-11-16 16:17:38 +0700

    remove meta remember-token

--- resources/views/Classic_hotel/dashboard.blade.php
+++ resources/views/Classic_hotel/dashboard.blade.php
@@ -9,7 +9,7 @@
     <?php
         $_COOKIE['remember-token']=Auth::user()->remember_token;
     ?>
-    <meta name="remember-token" content="{{ $_COOKIE['remember-token'] }}">
+
     <!-- jquery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
